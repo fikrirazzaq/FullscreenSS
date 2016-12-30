21f24045d42c0e0c1dd7f7abc57ccf581ea86afa
juvetic <fikrirazzaq30@gmail.com>
2016-12-30 11:44:57 +0700

    Fullscreen Screenshot Done!

---
+++ app/src/main/java/com/example/tel_c3/fullscreenss/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.tel_c3.fullscreenss;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Environment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    TextView textView;
+    ImageView imageView;
+    Bitmap mBitmap;
+    Button captureScreenShot;
+    private ShareActionProvider mShareActionProvider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setText("Your ScreenShot Image:");
+
+        captureScreenShot = (Button) findViewById(R.id.capture_screen_shot);
+        imageView = (ImageView) findViewById(R.id.imageView);
+    }
+
+    public void screenShot(View view) {
+        mBitmap = getBitmapOFRootView(captureScreenShot);
+        imageView.setImageBitmap(mBitmap);
+        createImage(mBitmap);
+    }
+
+    public Bitmap getBitmapOFRootView(View v) {
+        View rootview = v.getRootView();
+        rootview.setDrawingCacheEnabled(true);
+        Bitmap bitmap1 = rootview.getDrawingCache();
+        return bitmap1;
+    }
+
+    public void createImage(Bitmap bmp) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 40, bytes);
+        File file = new File(Environment.getExternalStorageDirectory() +
+                "/ezkieu.jpg");
+        try {
+            file.createNewFile();
+            FileOutputStream outputStream = new FileOutputStream(file);
+            outputStream.write(bytes.toByteArray());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actions, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        mShareActionProvider.setShareIntent(createShareIntent());
+        return true;
+    }
+
+    private Intent createShareIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, "#NuhunSTTTelkom");
+        return shareIntent;
+    }
+}
